b681cb4401e58857bc9566e11b81766e39e774db
Gaber11 <gaberezzat0@gmail.com>
2024-12-21 00:23:50 +0200

    2 - Polymorphism - Overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,45 @@
 {
     internal class Program
     {
+        static int SumTwo(int X , int Y)
+        {
+            return X+ Y;    
+        }
+        static int SumThree(int X, int Y,int Z)
+        {
+            return X + Y +Z;
+        }
+        static double SumTwoAndDouble(double X, int Y)
+        {
+            return X + Y;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region 2 - Polymorphism - Overloading
+
+          //  int Result1 = SumTwo(1, 2);
+          //  Console.WriteLine(Result1);
+
+          //int  Result2 = SumThree(1, 2, 3);
+          //  Console.WriteLine(Result2);
+
+          // double Result3 = SumTwoAndDouble(1.4, 2);
+          //  Console.WriteLine(Result3);
+          //  Console.WriteLine(true);
+          //  Console.WriteLine(1);
+          //  Console.WriteLine("Gaber");
+          //  Console.WriteLine();
+          //  Console.WriteLine(1.5);
+          //  Console.WriteLine(1.5f);
+          //  Console.WriteLine(1.5m);
+
+            #endregion
+
+
+
+
+
         }
     }
 }
