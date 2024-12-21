d56acd71c7441ab294f2a6acce3046ae87b1a001
Gaber11 <gaberezzat0@gmail.com>
2024-12-21 20:22:27 +0200

    Question 1:What is the primary purpose of an interface in C#?

---
+++ Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1:What is the primary purpose of an interface in C#?
+            //b) To define a blueprint for a class
+            #endregion
+
+
+
+
+        }
+    }
+}
